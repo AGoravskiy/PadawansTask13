3e24fbc012dabd418bba2df2cdbd292d7c994acd
Aleksey <Narchuk.Aleksey@gmail.com>
2019-02-28 20:44:38 +0300

    Full solution

---
+++ Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string surname;
+        private int age;
+
+        public Employee() { }
+        public Employee(string surname, int age)
+        {
+            if (age <= 0)
+            {
+                throw new ArgumentException();
+            }
+            this.surname = surname ?? throw new ArgumentNullException();
+            this.age = age;
+        }
+
+        private string ShowAge()
+        {
+            return age.ToString();
+        }
+        public void ChangeSurname(string newSurname)
+        {
+            this.surname = newSurname ?? throw new ArgumentNullException();
+        }
+        public override string ToString()
+        {
+            return "Surname: " + this.surname + ", Age: " + this.age;
+        }
+    }
+}
